72dd1bd7eaac15d5f8aca5d7641dd25fbb23f3fb
Shantanu Rajmane <shantanurajmane_22992@aitpune.edu.in>
2023-09-15 16:38:30 +0530

    Calendar Styling!

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,26 +1,48 @@
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import list from '@fullcalendar/list';
+import Fullcalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import React from 'react';
-import Fullcalendar from '@fullcalendar/react';
-import list from '@fullcalendar/list';
+import './calendar.css';
+
+
+const addEvent = () => {
+
+}
 
 const Calendar = () => {
     return (
-        <div>
-            <Fullcalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin,list]}
-                initialView={"dayGridMonth"}
-                headerToolbar={{
-                start: "today prev,next", // will normally be on the left. if RTL, will be on the right
-                center: "title",
-                end: "dayGridMonth,timeGridWeek,timeGridDay,list", // will normally be on the right. if RTL, will be on the left
-                }}
-                height={"90vh"}
-                windowResizeDelay={0}
-                selectable={true}
-                nowIndicator={true}
-            />
+        <div className='page'>
+            <div className='aside'>
+                <h2>AIT Event Hub</h2>
+                <Fullcalendar
+                    plugins={[list]}
+                    initialView='listWeek'
+                    headerToolbar={false}
+                />
+            </div>
+            <div className='calendar'>
+                <Fullcalendar
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    initialView={"dayGridMonth"}
+                    headerToolbar={{
+                    start: "prev,today,next addEventButton", // will normally be on the left. if RTL, will be on the right
+                    center: "title",
+                    end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+                    }}
+                    height={"100vh"}
+                    windowResizeDelay={0}
+                    selectable={true}
+                    nowIndicator={true}
+                    customButtons = {{
+                        addEventButton : {
+                            text: 'Add Event',
+                            click: addEvent()
+                        }
+                    }}
+                />
+            </div>
         </div>
     );
 }
